b19b4e8e52c1f0fee915349dd97c33aa47ba4e6f
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-15 20:19:40 -0500

    Logical operators...

--- Program.cs
+++ Program.cs
@@ -2,11 +2,61 @@
 
 namespace conditionals
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      int age = 17;
+
+      if ((age >= 5) && (age <= 7))
+      {
+        System.Console.WriteLine("This student should go to Elementary School");
+      }
+      else if ((age > 7) && (age < 13))
+      {
+        System.Console.WriteLine("This student should go to Middle School");
+      }
+      else if ((age > 13) && (age < 19))
+      {
+        System.Console.WriteLine("This student should go to High School");
+
+      }
+      else
+      {
+        System.Console.WriteLine("This student should go to College");
+
+      }
+
+
+      if (( age > 14 ) || ( age < 19)) {
+          System.Console.WriteLine("You should'nt work.");
+      }
+
+      bool canDrive = age >= 16;
+
+      switch (age) {
+          case 1:
+          case 2:
+            System.Console.WriteLine("Go to daycare");
+            break;
+            case 3:
+            case 4:
+                System.Console.WriteLine("Go to preschool");
+                break;
+            case 5:
+            System.Console.WriteLine("Go to kindergarten");
+            break;
+            default:
+                System.Console.WriteLine("Go to another school");
+      }
+
+
+      string name = "Derek";
+      string name2 = "Derek";
+
+      if (name.Equals(name2, StringComparison.Ordinal)) {
+          System.Console.WriteLine("The names are equal");
+      }
     }
+  }
 }
